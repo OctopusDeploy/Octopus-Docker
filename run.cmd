e4918f90fc21cc0c1070b33ab527c6d29a81a4d4
Matt Richardson <matt.richardson@octopus.com>
2016-08-31 15:55:13 +1000

    Use external data storage

--- run.cmd
+++ run.cmd
@@ -1,5 +1,16 @@
 ﻿
-cls
-docker run --net nat ^
-           -p 81:81 ^
+@cls
+@if not exist logs mkdir logs
+@if not exist artifacts mkdir artifacts
+@if not exist packagecache mkdir packagecache
+@if not exist packages mkdir packages
+@if not exist tasklogs mkdir tasklogs
+
+docker run -p 81:81 ^
+           -e SqlDbConnectionString='foo=bar' ^
+           -v c:/temp/Octopus/logs:c:/Octopus/Logs ^
+           -v c:/temp/Octopus/artifacts:c:/Octopus/aartifacts ^
+           -v c:/temp/Octopus/artifacts:c:/Octopus/OctopusServer/aartifacts ^
+           -v c:/temp/Octopus/packages:c:/Octopus/ppackages ^
+           -v c:/temp/Octopus/tasklogs:c:/Octopus/TaskLogs ^
            octopusdeploy/octopusdeploy:3.4.2
--- todo.md
+++ todo.md
@@ -1,8 +1,8 @@
 ﻿todo:
 * DONE ensure we can select which version in build.cmd
 * figure out how to pass a connection string as environment variable 
-* allow logs to go out of the container
-* allow artifact storage to go out of the container
+DONE * allow logs to go out of the container
+DONE * allow artifact storage to go out of the container
 * setup automated build
 * publish to docker hub?
 * tag as latest
