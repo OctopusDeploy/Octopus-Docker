e209e9202ccf825fdffd1db4851aff3cc2ca3d1d
Matt Richardson <matt@redgumtech.com.au>
2017-02-17 11:57:45 +1100

    Ensure we return proper exit codes

--- mssql-server-2014-express-windows/publish.ps1
+++ mssql-server-2014-express-windows/publish.ps1
@@ -7,6 +7,10 @@ param (
 
 write-host "docker login -u=`"$UserName`" -p=`"#########`""
 & docker login -u="$UserName" -p="$Password"
+if ($$LASTEXITCODE -ne 0) {
+  exit $LASTEXITCODE
+}
 
 write-host "docker push octopusdeploy/mssql-server-2014-express-windows:latest"
 & docker push octopusdeploy/mssql-server-2014-express-windows:latest
+exit $LASTEXITCODE
--- publish.ps1
+++ publish.ps1
@@ -9,6 +9,10 @@ param (
 
 write-host "docker login -u=`"$UserName`" -p=`"#########`""
 & docker login -u="$UserName" -p="$Password"
+if ($$LASTEXITCODE -ne 0) {
+  exit $LASTEXITCODE
+}
 
 write-host "docker push octopusdeploy/octopusdeploy-prerelease:$OctopusVersion"
 & docker push octopusdeploy/octopusdeploy-prerelease:$OctopusVersion
+exit $LASTEXITCODE
