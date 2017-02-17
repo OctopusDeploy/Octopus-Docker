f214e54f63ae7e9fcc0cdf4915af19558d1068d2
Matt Richardson <matt@redgumtech.com.au>
2017-02-17 11:57:31 +1100

    Make building images more resiliant in the face of flakey docker

--- build.ps1
+++ build.ps1
@@ -3,18 +3,69 @@ param (
   [string]$OctopusVersion
 )
 
+function Execute-Command ($commandPath, $commandArguments)
+{
+    Write-Host "Executing '$commandPath $commandArguments'"
+    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+    $pinfo.FileName = $commandPath
+    $pinfo.RedirectStandardError = $true
+    $pinfo.RedirectStandardOutput = $true
+    $pinfo.UseShellExecute = $false
+    $pinfo.Arguments = $commandArguments
+    $pinfo.WorkingDirectory = $pwd
+    $p = New-Object System.Diagnostics.Process
+    $p.StartInfo = $pinfo
+    $p.Start() | Out-Null
+    $stdout = $p.StandardOutput.ReadToEnd()
+    $stderr = $p.StandardError.ReadToEnd()
+    $p.WaitForExit()
+
+    Write-Host $stdout
+    Write-Host $stderr
+    Write-Host "Process exited with exit code $($p.ExitCode)"
+
+    [pscustomobject]@{
+        stdout = $stdout
+        stderr = $stderr
+        ExitCode = $p.ExitCode
+    }
+}
+
 write-host "------------"
 write-host "docker --version"
 write-host "------------"
 & docker --version
+if ($$LASTEXITCODE -ne 0) {
+  exit $LASTEXITCODE
+}
+
 write-host "------------"
 write-host "docker version"
 write-host "------------"
 & docker version
+if ($$LASTEXITCODE -ne 0) {
+  exit $LASTEXITCODE
+}
 write-host "------------"
 
 # todo: check to make sure there is an msi in the "source" directory
 
-write-host "executing 'docker build --tag octopusdeploy/octopusdeploy-prerelease:$OctopusVersion --build-arg OctopusVersion=$OctopusVersion ."
-
-& docker build --tag octopusdeploy/octopusdeploy-prerelease:$OctopusVersion --build-arg OctopusVersion=$OctopusVersion .
+$maxAttempts = 10
+$attemptNumber = 0
+while ($true) {
+  $attemptNumber = $attemptNumber + 1
+  write-host "Attempt #$attemptNumber to build container..."
+  $result = Execute-Command "docker" "build --tag octopusdeploy/octopusdeploy-prerelease:$OctopusVersion --build-arg OctopusVersion=$OctopusVersion"
+  if ($result.stderr -like "*encountered an error during Start: failure in a Windows system call: This operation returned because the timeout period expired. (0x5b4)*") {
+    if ($attemptNumber -gt $maxAttempts) {
+      write-host "Giving up after $attemptNumber attempts."
+      exit 1
+    }
+    write-host "Docker failed - retrying..."
+  } elseif ($result.ExitCode -ne 0) {
+    write-host "Docker failed with an unknown error. Aborting."
+    exit $result.ExitCode
+  } else {
+    exit 0
+  }
+}
