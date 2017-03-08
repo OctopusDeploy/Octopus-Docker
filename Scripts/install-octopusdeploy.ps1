10418d7ebe05eeddf6b4626b1b4e524cd7a57e53
Rob <robert.erez@gmail.com>
2017-03-08 17:39:09 +1000

    Update install-octopusdeploy.ps1

--- Scripts/install-octopusdeploy.ps1
+++ Scripts/install-octopusdeploy.ps1
@@ -3,7 +3,7 @@ Param()
 
 $version = $env:OctopusVersion
 $msiFileName = "Octopus.$($version)-x64.msi"
-$downloadBaseUrl = "https://download.octopusdeploy.com/octopus/"
+$downloadBaseUrl = "https://octopus-testing.s3.amazonaws.com/server/"
 $downloadUrl = $downloadBaseUrl + $msiFileName
 $installBasePath = "C:\Install\"
 $msiPath = $installBasePath + $msiFileName
