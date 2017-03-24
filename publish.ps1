65a11f40423ce12df561d78d5869f65fda568972
Rob <robert.erez@gmail.com>
2017-03-24 12:27:19 +1000

    Allow publish.ps1 to publish preview builds

--- publish.ps1
+++ publish.ps1
@@ -4,23 +4,79 @@ param (
   [Parameter(Mandatory=$true)]
   [string]$Password,
   [Parameter(Mandatory=$true)]		
-  [string]$OctopusVersion
+  [string]$OctopusVersion,
+  [Parameter(Mandatory=$true)]		
+  [bool]$Release = $false
 )
 
-write-host "docker login -u=`"$UserName`" -p=`"#########`""
-& docker login -u="$UserName" -p="$Password"
-if ($LASTEXITCODE -ne 0) {
-  exit $LASTEXITCODE
+function Docker-Login() {
+	write-host "docker login -u=`"$UserName`" -p=`"#########`""
+	& docker login -u="$UserName" -p="$Password"
+	if ($LASTEXITCODE -ne 0) {
+	  exit $LASTEXITCODE
+	}
 }
 
-write-host "docker push octopusdeploy/octopusdeploy-prerelease:$OctopusVersion"
-& docker push octopusdeploy/octopusdeploy-prerelease:$OctopusVersion
-if ($LASTEXITCODE -ne 0) {
-  exit $LASTEXITCODE
+function Get-ImageName() {
+param (
+	[Parameter(Mandatory=$true)]	
+	[ValidateSet("server","tentacle")]
+	[string]$AppType,
+	[Parameter(Mandatory=$true)]		
+	[string]$Version,
+	[Parameter(Mandatory=$true)]		
+	[bool]$Release = $false
+	)
+	
+	$image = "octopusdeploy/octopusdeploy"
+		
+	if($AppType -eq "tentacle"){
+		$image += "-tentacle";
+	}
+	
+	if($Release) {
+	 $image += "-preview"
+	} else {
+	 $image += "-prerelease"	
+	}
+	
+	return $image + ":$Version"
 }
 
-write-host "docker push octopusdeploy/octopusdeploy-tentacle-prerelease:$OctopusVersion"
-& docker push octopusdeploy/octopusdeploy-tentacle-prerelease:$OctopusVersion
-if ($LASTEXITCODE -ne 0) {
-  exit $LASTEXITCODE
+function Push-Image(){
+param (
+	[Parameter(Mandatory=$true)]
+	[string] $ImageName
+	)
+	
+	write-host "docker push $ImageName"
+	& docker push $ImageName
+	if ($LASTEXITCODE -ne 0) {
+	  exit $LASTEXITCODE
+	}
+}
+
+function TagRelease() {
+	Write-Host "docker tag $(Get-ImageName 'server' $OctopusVersion $False) $(Get-ImageName 'server' $OctopusVersion $True)"
+	& docker tag $(Get-ImageName 'server' $OctopusVersion $False) $(Get-ImageName 'server' $OctopusVersion $True)
+	
+	write-host "docker tag $(Get-ImageName 'tentacle' $OctopusVersion $False) $(Get-ImageName 'tentacle' $OctopusVersion $True)"
+	& docker tag $(Get-ImageName 'tentacle' $OctopusVersion $False) $(Get-ImageName 'tentacle' $OctopusVersion $True)
+}
+
+function Publish(){
+param(
+	[Parameter(Mandatory=$true)]		
+	[bool]$Release = $false	
+	)
+	Push-Image $(Get-ImageName "server" $OctopusVersion $Release)
+	Push-Image $(Get-ImageName "tentacle" $OctopusVersion $Release)	
+}
+
+#Docker-Login
+if($Release) {
+	TagRelease
+	Publish -Release $True
+} else {
+	Publish -Release $False
 }
