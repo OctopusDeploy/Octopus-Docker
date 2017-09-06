1e4457fbde0ca1d3ef310f8f15e908fe9538f052
Matt Richardson <matt.richardson@octopus.com>
2017-09-06 20:53:14 +1000

    Pass in version numbers so we can set image suffix

--- tests/stop.ps1
+++ tests/stop.ps1
@@ -1,7 +1,19 @@
 param (
   [Parameter(Mandatory=$false)]
-  [string]$ProjectName="octopusdocker"
+  [string]$ProjectName="octopusdocker",
+  [Parameter(Mandatory=$true)]
+  [string]$OctopusVersion,
+  [Parameter(Mandatory=$false)]
+  [string]$TentacleVersion
 )
+
+$IncludeTentacle = (($TentacleVersion -ne $null) -and ($TentacleVersion -ne ""))
+
+if ($IncludeTentacle) {
+  $env:OCTOPUS_IMAGE_SUFFIX = "preview"
+} else {
+  $env:OCTOPUS_IMAGE_SUFFIX = "prerelease"
+}
 
 write-host "Stopping '$ProjectName' compose project"
 & "C:\Program Files\Docker Toolbox\docker-compose" --project-name $ProjectName stop
