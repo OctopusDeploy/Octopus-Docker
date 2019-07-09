8cba11b44a982dddb6e90e32eb8922171fba53c8
Rob <robert.erez@gmail.com>
2019-07-09 16:47:29 +1000

    Change how base image is generated

--- Server/01-build.ps1
+++ Server/01-build.ps1
@@ -13,10 +13,12 @@ Confirm-RunningFromRootDirectory
 TeamCity-Block("Build") {
   $imageVersion = Get-ImageVersion $OctopusVersion $OSVersion
   Write-Host "Creating image with tag 'octopusdeploy/octopusdeploy-prerelease:$imageVersion'"
-  if ($OSVersion -lt "1809") {
-    $baseImage = "microsoft/windowsservercore"
+  if($OSVersion -eq "ltsc2016") {
+	$baseImage = "mcr.microsoft.com/dotnet/framework/runtime:4.7.2-windowsservercore-ltsc2016"
+  } if ($OSVersion -lt "1809") {
+    $baseImage = "microsoft/windowsservercore:$OSVersion"
   } else {
-    $baseImage = "mcr.microsoft.com/windows/servercore"
+    $baseImage = "mcr.microsoft.com/windows/servercore:$OSVersion"
   }
 
   docker build --pull --tag octopusdeploy/octopusdeploy-prerelease:$imageVersion --build-arg SERVERCORE_VERSION=$OSVersion --build-arg BASE_IMAGE=$baseImage --build-arg OctopusVersion=$OctopusVersion --file Server\Dockerfile .
